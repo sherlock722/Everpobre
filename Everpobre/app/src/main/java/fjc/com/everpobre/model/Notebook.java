fab927fa659572b805e887b95afac17cda029f1e
Javier Cazorla <sherlock722@gmail.com>
2016-05-05 00:45:12 +0200

    Creación inicial de la clase Notebook

---
+++ Everpobre/app/src/main/java/fjc/com/everpobre/model/Notebook.java
@@ -0,0 +1,25 @@
+package fjc.com.everpobre.model;
+
+/**
+ * Created by javier on 5/5/16.
+ */
+public class Notebook {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
